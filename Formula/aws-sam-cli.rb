38be64833ac4d6f6cab9041b32e52f9e8566eedc
Jacob Fuss <jfuss@users.noreply.github.com>
2019-05-20 13:05:32 -0700

    chore(bottles): Add bottles for 0.16.1 release

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -7,10 +7,10 @@ class AwsSamCli < Formula
   sha256 "2dd68800723c76f52980141ba704e105d77469b6ba465781fbc9120e8121e76c"
   head "https://github.com/awslabs/aws-sam-cli.git", :branch => "develop"
   bottle do
-    root_url "https://github.com/awslabs/aws-sam-cli/releases/download/v0.16.0/"
+    root_url "https://github.com/awslabs/aws-sam-cli/releases/download/v0.16.1/"
     cellar :any_skip_relocation
-    sha256 "0b9e391bac3ebd30f2acbe2a7c5aff345e1f4cfd7ed7d74c486ed7066dc97009" => :sierra
-    sha256 "d2659f89310e610c236d836ff5edbb467529cccc65131c507ba4e7d1e69d7d39" => :x86_64_linux
+    sha256 "3a03e68f15567d5f15ed559b93a2ac5f8b72315edb98bef779fe52730bfb3698" => :sierra
+    sha256 "996ef933e8df93c82ee92c0b5b48b92e9de3318c921c04a926640eda7a72cd94" => :x86_64_linux
   end
 
   depends_on "python"
