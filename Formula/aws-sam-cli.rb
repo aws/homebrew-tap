f8a437b1f7c378734210f6683b98dd77074b885e
Jacob Fuss <jfuss@users.noreply.github.com>
2019-12-02 16:17:51 -0600

    chore: Updating to SAM CLI v0.35.0

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -4,8 +4,8 @@ class AwsSamCli < Formula
 
   desc "AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications"
   homepage "https://github.com/awslabs/aws-sam-cli/"
-  url "https://files.pythonhosted.org/packages/2c/4f/cda33dc06da70c6db23aee18ac75185bdc1aac37d7b0aed85d084b61091d/aws-sam-cli-0.34.0.tar.gz"
-  sha256 "3336d53795608dcaaf565cf9c7817c80726af79b91138beba643e46fda62afd9"
+  url "https://files.pythonhosted.org/packages/9f/f1/124ecc5bedf0f3e7eaaae52d173e82ec25faaf965df76be97635a9d1413d/aws-sam-cli-0.35.0.tar.gz"
+  sha256 "1ed43d5ad695c98886128ea376e0a98c37a92f11144b060be1eb052797665b2f"
   head "https://github.com/awslabs/aws-sam-cli.git", :branch => "develop"
   bottle do
     root_url "https://github.com/awslabs/aws-sam-cli/releases/download/v0.34.0/"
