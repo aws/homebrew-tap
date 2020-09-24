78294c9b0a3c3c5b4821db8a0eef09273b9d37ca
Justin Garrison <justinleegarrison@gmail.com>
2020-09-24 13:02:01 -0700

    Fix formula class name

--- Formula/container-tools.rb
+++ Formula/container-tools.rb
@@ -1,4 +1,4 @@
-class AwsContainerTools < Formula
+class ContainerTools < Formula
     desc "Meta Package for common AWS Container tools"
     url "file:///dev/null"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
