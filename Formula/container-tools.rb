45e579717902b9c60baae877fe44d8fda103c195
Justin Garrison <justin@linux.com>
2020-09-28 10:07:13 -0700

    Add a meta formula for "container tools" (#137)
    * Add container-tools formula and table descriptions
    
    * Fix formula class name
    
    * Add missing formula fields
    
    * Had to quote version string
    
    * s/endent/undent
    
    * apparently undent is deprecated

---
+++ Formula/container-tools.rb
@@ -0,0 +1,23 @@
+class ContainerTools < Formula
+    desc "Meta Package for common AWS Container tools"
+    version "1.0.0"
+    homepage "https://github.com/aws/"
+    bottle :unneeded
+    url "file:///dev/null"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+    depends_on "awscli"
+    depends_on "eksctl"
+    depends_on "kubernetes-cli"
+    depends_on "aws/tap/copilot-cli"
+    depends_on "aws-iam-authenticator"
+    depends_on "docker-credential-helper-ecr"
+
+    def install
+        (bin+"awscontainertools").write <<-EOS
+            #!/bin/sh
+
+            echo "AWS Container Tools installed"
+        EOS
+    end
+end
