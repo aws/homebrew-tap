2e9e4be2dc35eefa82d63f33c9068ef44f9fae75
Daniel Smith <1657182+StormPooper@users.noreply.github.com>
2021-04-19 20:38:41 +0100

    fix(aws-sam-cli): brew style --fix (#188)

--- Formula/aws-sam-cli-rc.rb
+++ Formula/aws-sam-cli-rc.rb
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-require_relative '../ConfigProvider/config_provider'
+require_relative "../ConfigProvider/config_provider"
 
 class AwsSamCliRc < Formula
   include Language::Python::Virtualenv
 
-  config_provider = ConfigProvider.new('aws-sam-cli-rc')
+  config_provider = ConfigProvider.new("aws-sam-cli-rc")
 
   desc "AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications"
   homepage "https://github.com/awslabs/aws-sam-cli/"
-  url config_provider.url()
+  url config_provider.url
   sha256 config_provider.sha256
-  head "https://github.com/awslabs/aws-sam-cli.git", :branch => "release-v1.0.0"
-
-  conflicts_with 'aws-sam-cli', :because => "both install the 'sam' binary"
+  head "https://github.com/awslabs/aws-sam-cli.git", branch: "release-v1.0.0"
 
   bottle do
-    root_url config_provider.root_url()
-    cellar :any_skip_relocation
-    sha256 config_provider.sierra_hash() => :sierra
-    sha256 config_provider.linux_hash() => :x86_64_linux
+    root_url config_provider.root_url
+    sha256 cellar: :any_skip_relocation, sierra:       config_provider.sierra_hash
+    sha256 cellar: :any_skip_relocation, x86_64_linux: config_provider.linux_hash
   end
 
   depends_on "python@3.7"
+
+  conflicts_with "aws-sam-cli", because: "both install the 'sam' binary"
 
   def install
     venv = virtualenv_create(libexec, "python3.7")
--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-require_relative '../ConfigProvider/config_provider'
+require_relative "../ConfigProvider/config_provider"
 
 class AwsSamCli < Formula
   include Language::Python::Virtualenv
 
-  config_provider = ConfigProvider.new('aws-sam-cli')
+  config_provider = ConfigProvider.new("aws-sam-cli")
 
   desc "AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications"
   homepage "https://github.com/awslabs/aws-sam-cli/"
-  url config_provider.url()
+  url config_provider.url
   sha256 config_provider.sha256
-  head "https://github.com/awslabs/aws-sam-cli.git", :branch => "develop"
-
-  conflicts_with 'aws-sam-cli-rc', :because => "both install the 'sam' binary"
+  head "https://github.com/awslabs/aws-sam-cli.git", branch: "develop"
 
   bottle do
-    root_url config_provider.root_url()
-    cellar :any_skip_relocation
-    sha256 config_provider.sierra_hash() => :sierra
-    sha256 config_provider.linux_hash() => :x86_64_linux
+    root_url config_provider.root_url
+    sha256 cellar: :any_skip_relocation, sierra:       config_provider.sierra_hash
+    sha256 cellar: :any_skip_relocation, x86_64_linux: config_provider.linux_hash
   end
 
   depends_on "python@3.8"
+
+  conflicts_with "aws-sam-cli-rc", because: "both install the 'sam' binary"
 
   def install
     venv = virtualenv_create(libexec, "python3.8")
