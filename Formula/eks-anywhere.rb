dfeea77ebc2da96766369985ace3780f8a4914ef
Justin Garrison <justinleegarrison@gmail.com>
2021-09-08 08:24:43 -0700

    chore: fix eks-anywhere shas (#225)

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -7,14 +7,14 @@ class EksAnywhere < Formula
     on_macos do
       if Hardware::CPU.intel?
         url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/1/artifacts/eks-a/v0.5.0/darwin/eksctl-anywhere-v0.5.0-darwin-amd64.tar.gz"
-        sha256 "6280f406f5596df83e2b994f1279b0c4992559f29858d666c99e4f4e0043ba94"
+        sha256 "ed7790c706216b531a5f844529142d734b52b538d15dc4f6bc7cbf0e717ab0cd"
       end
     end
 
     on_linux do
       if Hardware::CPU.intel?
         url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/1/artifacts/eks-a/v0.5.0/linux/eksctl-anywhere-v0.5.0-linux-amd64.tar.gz"
-        sha256 "0c271984896d2ea7a7bdb8c380cf845f63b8e4a6b54a1c47bb3bc5acadfc9a3d"
+        sha256 "020ae9fb3b01070bda29e530d157089bf56e11245bde098118ea0350ed5f5ec4"
       end
     end
 
