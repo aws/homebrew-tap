0c0b6f7691e7a33500652e52b1c9e7ab4886bf99
Mitali Paygude <mpaygude@amazon.com>
2021-11-23 12:54:01 -0800

    chore: bump up EKSA version for v0.6.1 (#253)

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -1,16 +1,16 @@
 class EksAnywhere < Formula
   desc "CLI for managing EKS Anywhere Kubernetes clusters"
   homepage "https://github.com/aws/eks-anywhere"
-  version "0.6.0"
+  version "0.6.1"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/2/artifacts/eks-a/v0.6.0/darwin/eksctl-anywhere-v0.6.0-darwin-amd64.tar.gz"
-    sha256 "25d4cc5eab63de4f435dcca558d277d317c42db920b03366decca7d883cfbebe"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/3/artifacts/eks-a/v0.6.1/darwin/eksctl-anywhere-v0.6.1-darwin-amd64.tar.gz"
+    sha256 "f21d324b37954f483a3467018ec7cd85d7829a87a70a845462378bc48efbe1f2"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/2/artifacts/eks-a/v0.6.0/linux/eksctl-anywhere-v0.6.0-linux-amd64.tar.gz"
-    sha256 "a6c235b974821a35417141333e901c8e6a230238b5f3b8f70a0c3cad74bfc26c"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/3/artifacts/eks-a/v0.6.1/linux/eksctl-anywhere-v0.6.1-linux-amd64.tar.gz"
+    sha256 "9571f13abb08672acf0b144e4c21138d59d32cdcdf1e22b27a38e0fb5ebb6bfa"
   end
 
   if Hardware::CPU.arm?
