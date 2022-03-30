21d8ed1935cff0a3ba702216eb4363242ea50220
aws-viewer-for-cbmc-release-ci <aws-arg-cbmc-viewer-devs@amazon.com>
2022-03-30 19:09:09 +0000

    cbmc-viewer 2.12

--- Formula/cbmc-viewer.rb
+++ Formula/cbmc-viewer.rb
@@ -3,8 +3,8 @@ class CbmcViewer < Formula
   desc "Scans the output of CBMC and produces a browsable summary of the results"
   homepage "https://github.com/awslabs/aws-viewer-for-cbmc"
   url "https://github.com/awslabs/aws-viewer-for-cbmc.git",
-      tag:      "viewer-2.11",
-      revision: "33bdc901dcada996fdcf47e2258d4db4c27c6ee5"
+      tag:      "viewer-2.12",
+      revision: "7e47db35d8fc36997c3feaa69189497e5f65660d"
   license "Apache-2.0"
 
   depends_on "cbmc" => :test
