d2780b34b761e8b96e947cd5e6506b0e59a6554f
Qingchuan Ma <qingchm@amazon.com>
2022-04-05 14:11:47 -0700

    Removing the --pre flag with causes installation issues with newest cython version

--- Formula/aws-sam-cli-rc.rb
+++ Formula/aws-sam-cli-rc.rb
@@ -24,7 +24,7 @@ class AwsSamCliRc < Formula
   def install
     venv = virtualenv_create(libexec, "python3.7")
     system libexec/"bin/pip", "install", "pip==19.2.3"
-    system libexec/"bin/pip", "install", "-v", "--pre", "--ignore-installed", buildpath
+    system libexec/"bin/pip", "install", "-v", "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "aws-sam-cli"
     venv.pip_install_and_link buildpath
   end
