63f8c37fa2af9c0866e12eded96e1aecd41fb168
Rajashree Mandaogane <mandaor@amazon.com>
2022-04-08 14:12:50 -0700

    Update eks-anywhere formula for v0.8.1

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -1,16 +1,16 @@
 class EksAnywhere < Formula
   desc "CLI for managing EKS Anywhere Kubernetes clusters"
   homepage "https://github.com/aws/eks-anywhere"
-  version "0.8.0"
+  version "0.8.1"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/darwin/amd64/eksctl-anywhere-v0.8.0-darwin-amd64.tar.gz"
-    sha256 "146ae6646a9723a197e99f3f0b064535235c5418e40deb9e5c6896d649b12357"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/9/artifacts/eks-a/v0.8.1/darwin/amd64/eksctl-anywhere-v0.8.1-darwin-amd64.tar.gz"
+    sha256 "a66138587b21d562e1c8c8edbab4e194be4de83c8b83673e00e40f3d941ee87a"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/linux/amd64/eksctl-anywhere-v0.8.0-linux-amd64.tar.gz"
-    sha256 "e2a896cde24832f15bd183714060f1801da0171db9f1af394aff2162e2ad195c"  
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/9/artifacts/eks-a/v0.8.1/linux/amd64/eksctl-anywhere-v0.8.1-linux-amd64.tar.gz"
+    sha256 "3209e90879e87306d953e1d1ecafdcab06cf337ba5b350d166d31f1415c07ca1"
   end
 
   if Hardware::CPU.arm?
