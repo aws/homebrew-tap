d3acb5ebb6bfdacc5fbff049951c4dc28a2fe17a
Ronak Fofaliya <ronakf@amazon.com>
2022-04-12 14:24:18 -0400

    Add cbmc-starter-kit 1.0 brew formula

---
+++ Formula/cbmc-starter-kit.rb
@@ -0,0 +1,34 @@
+class CbmcStarterKit < Formula
+  include Language::Python::Virtualenv
+  desc "Templates for writing CBMC proofs for C code"
+  homepage "https://github.com/awslabs/aws-viewer-for-cbmc"
+  url "https://github.com/model-checking/cbmc-starter-kit",
+      tag:      "starterkit-1.0",
+      revision: "4ef675178c8c7402d21d1b548b4ba3508fe3dbf3"
+  license "MIT-0"
+
+  depends_on "python@3.9"
+  
+  resource "gitdb" do
+    url "https://files.pythonhosted.org/packages/fc/44/64e02ef96f20b347385f0e9c03098659cb5a1285d36c3d17c56e534d80cf/gitdb-4.0.9.tar.gz"
+    sha256 "bac2fd45c0a1c9cf619e63a90d62bdc63892ef92387424b855792a6cabe789aa"
+  end
+
+  resource "GitPython" do
+    url "https://files.pythonhosted.org/packages/d6/39/5b91b6c40570dc1c753359de7492404ba8fe7d71af40b618a780c7ad1fc7/GitPython-3.1.27.tar.gz"
+    sha256 "1c885ce809e8ba2d88a29befeb385fcea06338d3640712b59ca623c220bb5704"
+  end
+
+  resource "smmap" do
+    url "https://files.pythonhosted.org/packages/21/2d/39c6c57032f786f1965022563eec60623bb3e1409ade6ad834ff703724f3/smmap-5.0.0.tar.gz"
+    sha256 "c840e62059cd3be204b0c9c9f74be2c09d5648eddd4580d9314c3ecde0b30936"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "python3", "-c", "cbmc-starter-kit-update --version"
+  end
+end
