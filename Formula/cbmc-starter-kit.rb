f900c81130de36ca91133dc0650e038c828ce9a9
Mark R. Tuttle <mrtuttle@amazon.com>
2022-04-22 19:56:57 -0400

    Correct cbmc-starter-kit base completion installation.

--- Formula/cbmc-starter-kit.rb
+++ Formula/cbmc-starter-kit.rb
@@ -34,7 +34,7 @@ class CbmcStarterKit < Formula
 
   def install
     virtualenv_install_with_resources
-    bash_completion.install "etc/bash_completion.d/cbmc-starter-kit.sh"
+    bash_completion.install "src/cbmc_starter_kit/etc/bash_completion.d/cbmc-starter-kit.sh"
   end
 
   test do
