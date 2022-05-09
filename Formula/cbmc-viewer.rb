454a8dfa17cc33a5e6777d93ed5636ff9abc7c23
Mark R. Tuttle <mrtuttle@amazon.com>
2022-05-09 12:32:30 -0500

    cbmc-viewer: change python 3.9->3.7, remove universal-ctags

--- Formula/cbmc-viewer.rb
+++ Formula/cbmc-viewer.rb
@@ -15,8 +15,7 @@ class CbmcViewer < Formula
   end
 
   depends_on "cbmc" => :test
-  depends_on "python@3.9"
-  depends_on "universal-ctags"
+  depends_on "python@3.7"
 
   resource "Jinja2" do
     url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
