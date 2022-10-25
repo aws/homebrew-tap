2f4502a10b339160124170e01790d1046c78de1a
Haresh Nasit <hnnasit@amazon.com>
2022-10-25 10:16:00 -0700

    Updated linux bottle config and formula and removed ubuntu image

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -11,25 +11,45 @@ class AwsSamCli < Formula
   sha256 config_provider.sha256
   head "https://github.com/awslabs/aws-sam-cli.git", branch: "develop"
 
-  bottle do
-    root_url config_provider.root_url
-    sha256 cellar: :any_skip_relocation, sierra:       config_provider.sierra_hash
-    sha256 cellar: :any_skip_relocation, x86_64_linux: config_provider.linux_hash
-  end
-
-  depends_on "python@3.8"
-
   conflicts_with "aws-sam-cli-rc", because: "both install the 'sam' binary"
 
-  def install
-    venv = virtualenv_create(libexec, "python3.8")
-    system libexec/"bin/pip", "install", "--upgrade", "pip"
-    system libexec/"bin/pip", "install", "-v", "--ignore-installed", buildpath
-    system libexec/"bin/pip", "uninstall", "-y", "aws-sam-cli"
-    venv.pip_install_and_link buildpath
+  def self.is_native_binary_supported?
+    OS.linux? and Hardware::CPU.intel?
+  end
+
+  if AwsSamCli.is_native_binary_supported?
+    # instructions for native installer
+    on_linux do
+      on_intel do
+        url config_provider.native_root_url + config_provider.native_linux_x86_file
+        sha256 config_provider.native_linux_x86_hash
+      end
+    end
+
+    def install
+      libexec.install Dir["dist/*"]
+      bin.write_exec_script libexec/"sam"
+    end
+  else
+    # instructions for python virtualenv installer
+    bottle do
+      root_url config_provider.root_url
+      sha256 cellar: :any_skip_relocation, sierra:       config_provider.sierra_hash
+    end
+
+    depends_on "python@3.8"
+
+    def install
+      venv = virtualenv_create(libexec, "python3.8")
+      system libexec/"bin/pip", "install", "--upgrade", "pip"
+      system libexec/"bin/pip", "install", "-v", "--ignore-installed", buildpath
+      system libexec/"bin/pip", "uninstall", "-y", "aws-sam-cli"
+      venv.pip_install_and_link buildpath
+    end
   end
 
   test do
     assert_match "Usage", shell_output("#{bin}/sam --help")
+    system bin/"sam --version"
   end
 end
