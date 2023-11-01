c3fed30dcb0ad2603fd08cee978ff45f591a1754
Tanvir Tatla <tatlat@amazon.com>
2023-11-01 03:35:28 +0000

    Update eks-anywhere brew formula for v0.18.0

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -1,16 +1,16 @@
 class EksAnywhere < Formula
   desc "CLI for managing EKS Anywhere Kubernetes clusters"
   homepage "https://github.com/aws/eks-anywhere"
-  version "0.17.4"
+  version "0.18.0"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/49/artifacts/eks-a/v0.17.4/darwin/amd64/eksctl-anywhere-v0.17.4-darwin-amd64.tar.gz"
-    sha256 "71185fb7ec5f0ad40185d538397e06427e4462afdf85712fac158393ba1dc371"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/50/artifacts/eks-a/v0.18.0/darwin/amd64/eksctl-anywhere-v0.18.0-darwin-amd64.tar.gz"
+    sha256 "add3f52f639b86eef6c4a8e4f7b16dc60dc9360b698081c085c90e7e61f55e9d"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/49/artifacts/eks-a/v0.17.4/linux/amd64/eksctl-anywhere-v0.17.4-linux-amd64.tar.gz"
-    sha256 "388d3fd5632a0de10c765da07f10e573652df3458055d139e79be394584f2936"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/50/artifacts/eks-a/v0.18.0/linux/amd64/eksctl-anywhere-v0.18.0-linux-amd64.tar.gz"
+    sha256 "f6fe4f4a1592523193b76dcc228b76b1bc6bc715887562ba712cad236d4d37ad"
   end
 
   if Hardware::CPU.arm?
