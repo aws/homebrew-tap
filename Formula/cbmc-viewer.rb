1593e499d76018e9c221e272c478af748723878f
aws-viewer-for-cbmc-release-ci <aws-arg-cbmc-viewer-devs@amazon.com>
2024-07-19 08:35:23 +0000

    cbmc-viewer 3.9

--- Formula/cbmc-viewer.rb
+++ Formula/cbmc-viewer.rb
@@ -3,8 +3,8 @@ class CbmcViewer < Formula
   desc "Scans the output of CBMC and produces a browsable summary of the results"
   homepage "https://github.com/model-checking/cbmc-viewer"
   url "https://github.com/model-checking/cbmc-viewer.git",
-      tag:      "viewer-3.8",
-      revision: "f435f3f075b6cee95609f2f7138cad836c4c6192"
+      tag:      "viewer-3.9",
+      revision: "1141d2735d0a6dbfd61318eaba3c9044fa563b04"
   license "Apache-2.0"
 
   bottle do
